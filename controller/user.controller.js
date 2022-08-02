2e70e705c6db0bdbfe27cfa4fa6b5a8b358bd765
unknown <r26bisen@gmail.com>
2022-08-02 23:37:02 +0530

    timezone bug fix typo

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1071,8 +1071,8 @@ module.exports = {
 
   sendRequest: (req, res) => {
     const user_id = req.decoded.result.user_id
-    const booking_from = new Date(body.booking_from).getTime()
-    const booking_till = new Date(body.booking_till).getTime()
+    const booking_from = new Date(req.body.booking_from).getTime()
+    const booking_till = new Date(req.body.booking_till).getTime()
     addBookingRequest({ ...req.body,booking_from,booking_till, user_id }, (err, results) => {
       if (err) {
         console.log(err);
